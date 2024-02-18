896cc30298c1a1881c6cf167195af7ca2f95d839
Eleas75 <eleasmiah3@gmail.com>
2024-02-18 17:23:25 +0600

    functionality improved

--- ticketing.js
+++ ticketing.js
@@ -11,15 +11,29 @@ let count=0;
 const btns = document.getElementsByClassName("btn");
 for(const button of btns){
     button.addEventListener("click", function(e){
-        const count1=document.getElementById("ticketCount");innerText=count;
-        const count2=document.getElementById("remainTicket");innerText=40-count; 
-        count++
+        count++;
+        const count1=document.getElementById("ticketCount").innerText=count;
+        const count2=document.getElementById("remainTicket").innerText=40-count; 
+      
 e.target.style.backgroundColor="red";
 
 //publish ticket name
+const container =document.createElement("div")
 const tName=document.getElementById("ticketNaming");
 const ticketName=e.target.innerText;
 const p=document.createElement("p");
+const p1=document.createElement("p");
+const p2=document.createElement("p");
 p.innerText=ticketName;
-ticketNaming.appendChild(p);
+p1.innerText="second";
+p2.innerText="550TK";
+container.appendChild(p);
+container.appendChild(p1);
+container.appendChild(p2);
+tName.appendChild(container);
+container.classList.add("flex","flex-row","justify-between");
+
+
+
+
     })};
