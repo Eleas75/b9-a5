4ff99985e0b4d0f14c3007838165513e333e8540
Eleas75 <eleasmiah3@gmail.com>
2024-02-18 16:37:26 +0600

    functionality start

--- ticketing.js
+++ ticketing.js
@@ -3,5 +3,23 @@ document.getElementById("scrollButton").addEventListener("click", function() {
     var targetElement = document.getElementById("targetElement");
     
     // Scroll to the target element
-    targetElement.scrollIntoView({ behavior: 'instant', block: 'start' });
+    targetElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
+
+//get the buttons
+let count=0;
+const btns = document.getElementsByClassName("btn");
+for(const button of btns){
+    button.addEventListener("click", function(e){
+        const count1=document.getElementById("ticketCount");innerText=count;
+        const count2=document.getElementById("remainTicket");innerText=40-count; 
+        count++
+e.target.style.backgroundColor="red";
+
+//publish ticket name
+const tName=document.getElementById("ticketNaming");
+const ticketName=e.target.innerText;
+const p=document.createElement("p");
+p.innerText=ticketName;
+ticketNaming.appendChild(p);
+    })};
